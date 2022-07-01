4d2b9f6e33d74b3286ce0c24e6d24351b1e6b314
triiq <87587418+triiq@users.noreply.github.com>
2022-06-30 21:38:52 -0700

    Update index.tsx

--- pages/[stakePoolId]/index.tsx
+++ pages/[stakePoolId]/index.tsx
@@ -526,6 +526,7 @@ function Home() {
       <Head>
         <title>The Pumas</title>
         <meta name="description" content="The Pumas Staking" />
+        <meta property="og:url" content="https://pumas.triiq.xyz/purr" />
         <meta property="og:image" content="/banner.jpeg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
